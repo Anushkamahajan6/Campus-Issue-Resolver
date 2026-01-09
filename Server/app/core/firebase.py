7a34b8051561ec3ab9a35425f22f84563b2bfe48
Ajay Garg <ajaygarg200r@gmail.com>
2026-01-10 00:48:30 +0530

    bug fix

--- Server/app/core/firebase.py
+++ Server/app/core/firebase.py
@@ -8,6 +8,6 @@ firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 if not firebase_json:
     raise RuntimeError("FIREBASE_SERVICE_ACCOUNT env var not set")
 
-cred = credentials.Certificate(json.loads(firebasej))
+cred = credentials.Certificate(json.loads(firebase_json))
 initialize_app(cred)
 db = firestore.client()
