4fa712fbc3d2a66a114b1e004f543e9134d59d92
Ajay Garg <ajaygarg200r@gmail.com>
2026-01-10 00:44:18 +0530

    bug fix

--- Server/app/core/firebase.py
+++ Server/app/core/firebase.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-firebasej = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 
-if not firebasej:
+if not firebase_json:
     raise RuntimeError("FIREBASE_SERVICE_ACCOUNT env var not set")
 
-cred = credentials.Certificate(json.loads(firebasej))
+cred = credentials.Certificate(json.loads(firebase_json))
 initialize_app(cred)
 db = firestore.client()
 
